0a254924a5e99de5e5255ba336f9ec84d0cd83bf
Katherine <krosenf@gmail.com>
2024-04-13 18:45:04 +0000

    create local package

---
+++ src/src/standards.py
@@ -0,0 +1,14 @@
+"""
+name standards
+"""
+
+import sciris as sc
+
+class DataNames(sc.prettyobj):
+    cases: str = 'cases'
+    year: str = 'year'
+    iso: str = 'iso3'
+    country: str = 'country'
+    region: str = 'region'
+    cleaned: str = 'cleaned'
+        
